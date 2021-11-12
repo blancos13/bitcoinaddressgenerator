6bdb93fe294ec8ed09b731ca5b4d392cecf5905b
Altanb21 <94198465+Altanb21@users.noreply.github.com>
2021-11-12 23:28:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using NBitcoin;
+
+public class Program
+{
+
+	public static int _walletamount = 100000;
+
+	public static void Main()
+	{
+
+		HashSet<Key> hashSet = new HashSet<Key>();
+		for (int i = 0; i < _walletamoutn; i++)
+		{
+			Key item = new Key();
+			hashSet.Add(item);
+		}
+		if (!Directory.Exists("INFO"))
+		{
+			Directory.CreateDirectory("INFO");
+		}
+		if (File.Exists("INFO\\address.txt"))
+		{
+			File.Delete("INFO\\address.txt");
+		}
+		if (File.Exists("INFO\\secretkey.txt"))
+		{
+			File.Delete("INFO\\secrekey.txt");
+		}
+		using (StreamWriter streamWriter = File.CreateText("INFO\\address.txt"))
+		{
+			using (StreamWriter streamWriter2 = File.CreateText("INFO\\secretkey.txt"))
+			{
+				foreach (Key key in hashSet)
+				{
+					string text = key.PubKey.GetAddress(ScriptPubKeyType.Legacy, Network.Main).ToString();
+					streamWriter.WriteLine(text);
+					Console.WriteLine(text + ":" + key.GetBitcoinSecret(Network.Main) + "  " + System.DateTimeOffset.Now.ToString("hh:mm:ss.fff"));
+				}
+				streamWriter.Flush();
+				streamWriter2.Flush();
+			}
+		}
+	}
+}
---
+++ Wallet.cs
@@ -0,0 +1,5 @@
+﻿public class Wallet
+{
+    public string PublicKey { get; internal set; }
+    public string PrivateKey { get; internal set; }
+}
